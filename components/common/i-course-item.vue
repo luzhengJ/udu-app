f6982ddeea2894eef08dac6b97250819d2d4d442
鲁正佼 <3038413299@qq.com>
2022-11-10 22:19:22 +0800

    feat(新功能):对热门推荐模块的课程列表进行组件封装

--- components/common/i-course-item.vue
+++ components/common/i-course-item.vue
@@ -1,23 +1,55 @@
 <template>
 	<view class="course-item">
 		<view class="item-left">
-			<image class="course-cover" src="https://10.idqqimg.com/qqcourse_logo_ng/ajNVdqHZLLBctGyNKeVFHvodavrFNyoPnicMiczYSFpAz3Eu17nibpDe0VgSCswPhIOLGQnO7K1hp4/510" ></image>
+			<image class="course-cover" :src="item.mainImage"></image>
 			<view class="course-time">
-				06:55:54
+				{{item.totalTime}}
 			</view>
 		</view>
-		<view class="item-right">right</view>
+		<view class="item-right">
+			<view class="title text-ellipsis">{{item.title}}</view>
+			<view class="info">
+				<view class="nickname iconfont icon-laoshi2">{{item.nickName}}</view>
+				<view class="count">
+					<view v-if="item.isFree === 1" class="money iconfont icon-moneybag">免费</view>
+				  <view v-else class="money iconfont icon-moneybag">
+					{{item.priceOriginal || item.priceDiscount}}
+				  </view>
+					<view class="iconfont icon-video">{{item.studyTotal}}人在学</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
+	export default{
+		props:{
+			item : {
+				type : Object,
+				default : () => {
+					return {
+						id: 1,
+						mainImage: 'http://10.idqqimg.com/qqcourse_logo_ng/ajNVdqHZLLAWb3qFGBhykjmcTvz9CWmwib2Qj7c3Vxjia4y5fgSoNdrMYIdH11Dl1OCraibA7u0mts/600',
+						totalTime: '00:59:08',
+						title: 'SpringBoot项目实战教程SpringCloud OAuth2 Vue分布式微服务架构',
+						nickName: '梦老师1', // 课程老师
+						isFree: 0, //是否免费：0收费，1免费
+						priceOriginal: 999, // 原价
+						priceDiscount: 699.9, // 优惠价
+						studyTotal: 899
+					}
+				}
+			}
+		}
+	}
 </script>
 
 <style lang="scss">
 .course-item{
 	display: flex;
 	padding: 20rpx 0;
-	border-bottom: 1rpx solid #f60;
+	border-bottom: 1rpx solid #F1F1F1;
 	
 	.item-left{
 		width: 290rpx;
@@ -40,6 +72,46 @@
 			border-radius: 20rpx;
 			font-size: 22rpx;
 			padding :0 6px;
+		}
+	}
+	
+	.item-right{
+		height: 160rpx;
+		justify-content: space-between;
+		padding-left: 5rpx;
+		padding-top: 5rpx;
+		overflow: hidden;
+		.title{
+			max-width: 365rpx;
+			height: 70rpx;
+			line-height: 35rpx;
+			font-size: 28rpx;
+			font-weight: bold;
+		}
+		
+		.info{
+			display: flex;
+			flex-direction: column;
+			
+			.nickname {
+				font-size: 23rpx;
+				color: #999;
+				margin: 20rpx 0 8rpx 0;
+			}
+			
+			.count{
+				display: flex;
+				align-items: center;
+				.iconfont{
+					font-size: 23rpx;
+					color: #222222;
+				}
+				.money{
+					font-size: 28rpx;
+					color: $i-color-orange;
+					margin-right: 30rpx;
+				}
+			}
 		}
 	}
 }
--- pages/index/components/swiper-course.vue
+++ pages/index/components/swiper-course.vue
@@ -1,7 +1,16 @@
 <template>
 	<view>
 		<list-box>
-			<i-course-item v-for="(item,index) in 8" :key="index"></i-course-item>
+			<swiper 
+			class="list-swiper"
+			:style="{height:`${200 * rows}rpx`}"
+			>
+				<swiper-item v-for="(n,i) in column" :key="i">
+					<view class="swiper-item">
+						<i-course-item :item="item" v-for="(item,index) in courseData.slice(i*rows,(i+1)*rows)" :key="item.id"></i-course-item>
+					</view>
+				</swiper-item>
+			</swiper>
 		</list-box>
 	</view>
 </template>
@@ -9,7 +18,25 @@
 <script>
 	import listBox from "@/pages/index/components/list-box.vue"
 	import iCourseItem from "@/components/common/i-course-item.vue"
+	import courseData from "@/mock/courseData.js"
 	export default {
+		props:{
+			// 当前数据的列数(有几列)
+			column : {
+			    type : Number,
+				default : 3
+			},
+			// 当前数据每页的行数
+			rows : {
+				type : Number,
+				default : 5
+			},
+			// 列表数据
+			courseData : {
+				type : Array,
+				default : () => courseData
+			}
+		},
 		data() {
 			return {
 				
